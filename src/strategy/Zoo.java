01bc6e1a50f93f46c88f45177c2323777ff5fdc5
Adrian Ko <adrianchko@gmail.com>
2015-01-18 12:48:36 +0000

    Strategy explanation

--- src/strategy/Zoo.java
+++ src/strategy/Zoo.java
@@ -2,6 +2,12 @@ package strategy;
 
 public class Zoo {
 
+    /**
+     * Strategy pattern - when one of several behaviours is required dynamically
+     * among numerous subclasses without required the duplication of methods within
+     * those subclasses
+     * @param args
+     */
     public static void main(String[] args) {
         Animal spot = new Dog();
         Animal squark = new Bird();
