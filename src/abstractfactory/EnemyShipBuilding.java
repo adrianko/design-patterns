4e6d02e794fe55dd6e9e161d3cfb6931e8aa3239
Adrian Ko <adrianchko@gmail.com>
2015-01-18 15:09:11 +0000

    Add abstract EnemyShipBuilding

---
+++ src/abstractfactory/EnemyShipBuilding.java
@@ -0,0 +1,18 @@
+package abstractfactory;
+
+public abstract class EnemyShipBuilding {
+
+    protected abstract EnemyShip makeEnemyShip(String type);
+
+    public EnemyShip orderShip(String type) {
+        EnemyShip es = makeEnemyShip(type);
+
+        es.makeShip();
+        es.displayEnemyShip();
+        es.followHeroShip();
+        es.shoot();
+
+        return es;
+    }
+
+}
