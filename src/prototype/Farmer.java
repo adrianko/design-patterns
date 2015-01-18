6353d6ea1018fed98d713e2974025374816f88c9
Adrian Ko <adrianchko@gmail.com>
2015-01-18 17:26:14 +0000

    Add farmer for prototype pattern

---
+++ src/prototype/Farmer.java
@@ -0,0 +1,13 @@
+package prototype;
+
+public class Farmer {
+
+    /**
+     * For creating new objects by cloning other objects
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
