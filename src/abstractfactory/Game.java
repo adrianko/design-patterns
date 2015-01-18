e9af2f44b48f6ecc63627518b9db6fa06f9bd11d
Adrian Ko <adrianchko@gmail.com>
2015-01-18 15:53:15 +0000

    Abstract factory explanation

--- src/abstractfactory/Game.java
+++ src/abstractfactory/Game.java
@@ -3,7 +3,8 @@ package abstractfactory;
 public class Game {
 
     /**
-     *
+     * Every step is separated by either an abstract class or an interface making
+     * it possible to make extremely flexible objects.
      * @param args
      */
     public static void main(String[] args) {
