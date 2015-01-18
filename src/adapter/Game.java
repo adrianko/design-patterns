69ac81845dd6f03c3c17c1653890b7ce50617ae3
Adrian Ko <adrianchko@gmail.com>
2015-01-18 20:31:53 +0000

    Adapter explanation

--- src/adapter/Game.java
+++ src/adapter/Game.java
@@ -2,6 +2,14 @@ package adapter;
 
 public class Game {
 
+    /**
+     * Adapter - allows 2 incompatible interfaces to work together. Used when client
+     * expects to use a target interface. Adapter class allows the use of the available
+     * interface with Target interface.
+     * In this case the RobotAdapter maps expected methods to methods found within
+     * the EnemyRobot class.
+     * @param args
+     */
     public static void main(String[] args) {
         EnemyTank et = new EnemyTank();
         EnemyRobot er = new EnemyRobot();
