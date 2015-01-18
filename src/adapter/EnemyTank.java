388ce1100381fb27009376ba7ccb679771b3b4a1
Adrian Ko <adrianchko@gmail.com>
2015-01-18 20:21:57 +0000

    EnemyTank implementing EnemyAttacker

---
+++ src/adapter/EnemyTank.java
@@ -0,0 +1,24 @@
+package adapter;
+
+import java.util.Random;
+
+public class EnemyTank implements EnemyAttacker {
+
+    Random gen = new Random();
+
+    @Override
+    public void fireWeapon() {
+        System.out.println("Enemy Tank does " + (gen.nextInt(10) + 1) + " damage");
+    }
+
+    @Override
+    public void driveForward() {
+        System.out.println("Enemy Tank moves " + (gen.nextInt(5) + 1) + " spaces");
+    }
+
+    @Override
+    public void assignDriver(String d) {
+        System.out.println(d + " is driving the tank");
+    }
+
+}
