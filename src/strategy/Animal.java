5559850561ce4901f7b142475cfe1024f4055e37
Adrian Ko <adrianchko@gmail.com>
2015-01-18 12:35:25 +0000

    Subclasses can try to fly

--- src/strategy/Animal.java
+++ src/strategy/Animal.java
@@ -4,6 +4,10 @@ public class Animal {
 
     public Flys flyingType;
 
+    public String tryToFly() {
+        return flyingType.fly();
+    }
+
     public void setFlyingType(Flys newFlyType) {
         flyingType = newFlyType;
     }
