a33e030cf986f666677525d0dbef95833f211165
Adrian Ko <adrianchko@gmail.com>
2015-01-18 15:31:24 +0000

    Add runner method

--- src/abstractfactory/Game.java
+++ src/abstractfactory/Game.java
@@ -7,7 +7,12 @@ public class Game {
      * @param args
      */
     public static void main(String[] args) {
+        EnemyShipBuilding esb = new UFOEnemyShipBuilding();
+        EnemyShip es = esb.orderShip("UFO");
+        System.out.println(es);
 
+        EnemyShip boss = esb.orderShip("UFO BOSS");
+        System.out.println(boss);
     }
 
 }
