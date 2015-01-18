bf06693028859711d6c1afe37adb75318fc17d20
Adrian Ko <adrianchko@gmail.com>
2015-01-18 21:28:32 +0000

    Pizzaria runner

---
+++ src/decorator/Pizzaria.java
@@ -0,0 +1,14 @@
+package decorator;
+
+public class Pizzaria {
+
+    /**
+     * Decorator - allows modification of an object dynamically. Capabilities
+     * of inheritance with subclasses but can add functionality at runtime.
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
