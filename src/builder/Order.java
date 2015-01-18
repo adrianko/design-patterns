bef87451c200ab9f5e26dd917ac0d2865d502bb3
Adrian Ko <adrianchko@gmail.com>
2015-01-18 23:49:46 +0000

    Builder explanation

--- src/builder/Order.java
+++ src/builder/Order.java
@@ -1,7 +1,13 @@
 package builder;
 
 public class Order {
-    
+
+    /**
+     * Builder - many classes help in the creation of an object. By having different 
+     * classes build the object, many different types of objects can be created
+     * without forced rewriting of code.
+     * @param args
+     */
     public static void main(String[] args) {
         RobotEngineer re = new RobotEngineer(new OldRobotBuilder());
         re.makeRobot();
