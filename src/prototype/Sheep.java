f66693ac2dfd5100b14f16773b2275dee662d854
Adrian Ko <adrianchko@gmail.com>
2015-01-18 17:30:22 +0000

    Sheep class concrete

---
+++ src/prototype/Sheep.java
@@ -0,0 +1,26 @@
+package prototype;
+
+public class Sheep implements Animal {
+
+    public Sheep() {
+        System.out.println("Sheep is made");
+    }
+
+    public Animal makeCopy() {
+        System.out.println("Making sheep");
+        Sheep s = null;
+
+        try {
+            s = (Sheep) super.clone();
+        } catch(CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+        return s;
+    }
+
+    public String toString() {
+        return "I am a sheep";
+    }
+
+}
