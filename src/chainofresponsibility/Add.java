c5f4df2e85123b71dd27966e3d5c651f7934f678
Adrian Ko <adrianchko@gmail.com>
2015-01-19 15:19:04 +0000

    Start of chain

---
+++ src/chainofresponsibility/Add.java
@@ -0,0 +1,21 @@
+package chainofresponsibility;
+
+public class Add implements Chain {
+    
+    private Chain next;
+    
+    @Override
+    public void setNext(Chain n) {
+        next = n;
+    }
+
+    @Override
+    public void calculate(Numbers r) {
+        if (r.getCalculation().equals("add")) {
+            System.out.println(r.getN1() + " + " + r.getN2() + " = " + r.getN1() + r.getN2());
+        } else {
+            next.calculate(r);
+        }
+    }
+    
+}
