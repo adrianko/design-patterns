bfecf600416d4a80cfb9c579c57b20f1778136ed
Adrian Ko <adrianchko@gmail.com>
2015-01-19 15:21:11 +0000

    add multiple to chain

---
+++ src/chainofresponsibility/Multiply.java
@@ -0,0 +1,21 @@
+package chainofresponsibility;
+
+public class Multiply implements Chain {
+
+    private Chain next;
+
+    @Override
+    public void setNext(Chain n) {
+        next = n;
+    }
+
+    @Override
+    public void calculate(Numbers r) {
+        if (r.getCalculation().equals("mult")) {
+            System.out.println(r.getN1() + " * " + r.getN2() + " = " + (r.getN1() * r.getN2()));
+        } else {
+            next.calculate(r);
+        }
+    }
+    
+}
