db566691064ea74680678a161ec79bf0d4856655
Adrian Ko <adrianchko@gmail.com>
2015-01-19 14:54:28 +0000

    Facade pattern explainer

---
+++ src/facade/Customer.java
@@ -0,0 +1,15 @@
+package facade;
+
+public class Customer {
+
+    /**
+     * Facade - simplify methods so that much of what is done is in the background.
+     * Decouple client from sub components needed to perform an operation.
+     * A simplified interface that performs many other actions behind the scenes.
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
