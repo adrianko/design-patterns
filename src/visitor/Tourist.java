6114110e71b1cae04e0b74a4bb2db02fef869c2c
Adrian Ko <adrianchko@gmail.com>
2015-01-19 16:14:23 +0000

    Visitor runner

---
+++ src/visitor/Tourist.java
@@ -0,0 +1,15 @@
+package visitor;
+
+public class Tourist {
+
+    /**
+     * Allows addition of methods to classes of different types without much
+     * altering those classes. Can make completely different methods depending
+     * on the class used with this pattern
+     * @param args
+     */
+    public static void main(String[] args) {
+               
+    }
+    
+}
