463e8edc2dc54108ee032c964c8a310f9e01dd55
Adrian Ko <adrianchko@gmail.com>
2015-01-19 15:13:24 +0000

    Chain of Responsibility runner

---
+++ src/chainofresponsibility/Calculator.java
@@ -0,0 +1,15 @@
+package chainofresponsibility;
+
+public class Calculator {
+
+    /**
+     * A group of objects are expected to between them be able to solve a problem,
+     * if the first object can't solve it, it passes the data to the next object 
+     * in the chain.
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
