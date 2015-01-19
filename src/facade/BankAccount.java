89e06d1e7c2303ab240019098f716370b18bc49e
Adrian Ko <adrianchko@gmail.com>
2015-01-19 15:07:02 +0000

    BankAccount methods

---
+++ src/facade/BankAccount.java
@@ -0,0 +1,42 @@
+package facade;
+
+public class BankAccount {
+    
+    private int acc;
+    private int pin;
+    
+    CheckAccountNumber can;
+    CheckPIN cp;
+    CheckFunds cf;
+    
+    Welcome w;
+    
+    public BankAccount(int an, int p) {
+        acc = an;
+        pin = p;
+        
+        w = new Welcome();
+        
+        can = new CheckAccountNumber();
+        cp = new CheckPIN();
+        cf = new CheckFunds();
+    }
+    
+    public void withdrawCash(double cash) {
+        if (can.accountActive(acc) && cp.match(pin) && cf.enough(cash)) {
+            System.out.println("Transaction complete.");
+        } else {
+            System.out.println("Transaction failed.");
+        }
+    }
+    
+    public void depositCash(double cash) {
+        if (can.accountActive(acc) && cp.match(pin)) {
+            cf.makeDeposit(cash);
+            System.out.println("Transaction complete.");
+        } else {
+            System.out.println("Transaction failed.");
+        }
+    }
+    
+}
