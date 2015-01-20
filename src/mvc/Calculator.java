e5bef0f60eab31faec260f5385e7786eb7df78a9
Adrian Ko <adrianchko@gmail.com>
2015-01-20 22:56:00 +0000

    Start MVC explanation

---
+++ src/mvc/Calculator.java
@@ -0,0 +1,16 @@
+public class Calculator {
+
+    /**
+     * MVC separates caculations and data from the interface. Model contains the
+     * data and the methods to use the data. The View is the interface. The
+     * COntroller coordinates interactions between the Model and View.
+     * @param args
+     */
+    public static void main(String[] args) {
+        View v = new View();
+        Model m = new Model();
+        Controller c = new Controller(v, m);
+        v.setVisible(true);
+    }
+
+}
