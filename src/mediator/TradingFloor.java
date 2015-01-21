02c60857fc7a535f74e369f1b3af267da4860044
Adrian Ko <adrianchko@gmail.com>
2015-01-21 22:50:58 +0000

    Start mediator pattern

---
+++ src/mediator/TradingFloor.java
@@ -0,0 +1,15 @@
+package mediator;
+
+public class TradingFloor {
+
+    /**
+     * Mediator - used to handle communication between related objects (Colleagues).
+     * All communication is handled by a mediator object and the colleagues don't 
+     * need to know anything about each other to work together.
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
