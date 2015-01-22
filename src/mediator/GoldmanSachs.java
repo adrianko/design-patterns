4e4878b37eecfc199980f801c8d76d5b102f084b
Adrian Ko <adrianchko@gmail.com>
2015-01-22 13:22:40 +0000

    Add GoldmanSachs colleague

---
+++ src/mediator/GoldmanSachs.java
@@ -0,0 +1,10 @@
+package mediator;
+
+public class GoldmanSachs extends Colleague {
+    
+    public GoldmanSachs(Mediator m) {
+        super(m);
+        System.out.println("Goldman Sachs signed up with the exchange");
+    }
+    
+}
