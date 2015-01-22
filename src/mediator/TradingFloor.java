f195001a3e0bd73ebbcd6f486b12e607a0823cc8
Adrian Ko <adrianchko@gmail.com>
2015-01-22 09:41:36 +0000

    Add tradingfloor runtime

--- src/mediator/TradingFloor.java
+++ src/mediator/TradingFloor.java
@@ -9,7 +9,19 @@ public class TradingFloor {
      * @param args
      */
     public static void main(String[] args) {
+        StockMediator lse = new StockMediator();
+        MorganStanley b1 = new MorganStanley(lse);
+        RBS b2 = new RBS(lse);
         
+        b1.saleOffer("MSFT", 100);
+        b1.saleOffer("GOOG", 50);
+        
+        b2.buyOffer("MSFT", 100);
+        b2.saleOffer("NRG", 10);
+        
+        b1.buyOffer("NRG", 10);
+        
+        lse.getStockOffering();
     }
     
 }
