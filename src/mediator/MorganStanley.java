42f56641d791c940a0533b849807dd3e730298b6
Adrian Ko <adrianchko@gmail.com>
2015-01-22 09:22:00 +0000

    Add MorganStanley colleague

---
+++ src/mediator/MorganStanley.java
@@ -0,0 +1,11 @@
+package mediator;
+
+public class MorganStanley extends Colleague {
+
+    public MorganStanley(Mediator m) {
+        super(m);
+
+        System.out.println("Morgan Stanley signed up with the exchange.");
+    }
+    
+}
