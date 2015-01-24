80ddfb603aaa39274c5b809230bb3a646ea88d2c
Adrian Ko <adrianchko@gmail.com>
2015-01-24 11:38:55 +0000

    Add DJ composite to hold all components

--- src/composite/DJ.java
+++ src/composite/DJ.java
@@ -1,4 +1,15 @@
 package composite;
 
 public class DJ {
+    
+    SongComponent songList;
+    
+    public DJ(SongComponent sl) {
+        songList = sl;
+    }
+    
+    public void getSongList() {
+        songList.displaySongInfo();       
+    }
+    
 }
