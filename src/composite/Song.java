8b961097765eafc5a7fba31850e24606d7a24aa5
Adrian Ko <adrianchko@gmail.com>
2015-01-24 11:37:39 +0000

    Song methods overriding default

--- src/composite/Song.java
+++ src/composite/Song.java
@@ -1,4 +1,35 @@
 package composite;
 
 public class Song extends SongComponent {
+    
+    String songName;
+    String artist;
+    int releaseYear;
+    
+    public Song(String sn, String ar, int ry) {
+        songName = sn;
+        artist = ar;
+        releaseYear = ry;
+    }
+    
+    @Override
+    public String getSongName() {
+        return songName;
+    }
+    
+    @Override
+    public String getArtist() {
+        return artist;
+    }
+    
+    @Override
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+    
+    @Override
+    public void displaySongInfo() {
+        System.out.println(getSongName() + " was recorded by  " + getArtist() + " in " + getReleaseYear());
+    }
+    
 }
