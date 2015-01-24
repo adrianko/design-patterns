a936b82905bf17709ce79159ddd58297610659ab
Adrian Ko <adrianchko@gmail.com>
2015-01-24 11:41:31 +0000

    Add genres into global list

--- src/composite/Playlist.java
+++ src/composite/Playlist.java
@@ -6,6 +6,14 @@ public class Playlist {
         SongComponent pop = new SongGroup("Pop", "Popular music");
         SongComponent rock = new SongGroup("Rock", "Rock music");
         SongComponent rap = new SongGroup("Rap", "Rap music");
+        
+        SongComponent allSongs = new SongGroup("Song List", "All");
+        
+        allSongs.add(pop);
+        allSongs.add(rock);
+        allSongs.add(rap);
+        
+        
     }
     
 }
