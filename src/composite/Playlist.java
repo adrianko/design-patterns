7ad7e20d9a1bbec4cff611e2084b021c4042b229
Adrian Ko <adrianchko@gmail.com>
2015-01-24 11:40:23 +0000

    Add base groups/genres

--- src/composite/Playlist.java
+++ src/composite/Playlist.java
@@ -1,4 +1,11 @@
 package composite;
 
 public class Playlist {
+    
+    public static void main(String[] args) {
+        SongComponent pop = new SongGroup("Pop", "Popular music");
+        SongComponent rock = new SongGroup("Rock", "Rock music");
+        SongComponent rap = new SongGroup("Rap", "Rap music");
+    }
+    
 }
