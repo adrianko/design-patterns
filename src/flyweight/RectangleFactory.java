4df8e575224176492d16e4e6bbe003a6f3908097
Adrian Ko <adrianchko@gmail.com>
2015-01-25 13:58:06 +0000

    Add stashing for rectangles

---
+++ src/flyweight/RectangleFactory.java
@@ -0,0 +1,21 @@
+package flyweight;
+
+import java.awt.*;
+import java.util.HashMap;
+
+public class RectangleFactory {
+    
+    private static final HashMap<Color, Rectangle> rectangles = new HashMap<>();
+    
+    public static Rectangle getRectangle(Color c) {
+        Rectangle r = (Rectangle) rectangles.get(c);
+        
+        if (r == null) {
+            r = new Rectangle(c);
+            
+            rectangles.put(c, r);
+        }
+        
+        return r;
+    }
+}
