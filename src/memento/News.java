79856bcb644856789427643bb4bb2db572ac8a32
Adrian Ko <adrianchko@gmail.com>
2015-01-26 09:33:40 +0000

    Store first memento

--- src/memento/News.java
+++ src/memento/News.java
@@ -3,10 +3,14 @@ package memento;
 public class News {
     
     public static void main(String[] args) {
-        int currArticle, saveFiles = 0;
+        int currArticle = 0, saveFiles = 0;
         Caretaker c = new Caretaker();
         Originator o = new Originator();
         
+        o.set("Some text");
+        c.add(o.store());
+        currArticle++;
+        saveFiles++;
     }
     
 }
