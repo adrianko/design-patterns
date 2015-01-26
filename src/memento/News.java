aaa7d8d7a83b7fe4677561830662b43ad36f8e1f
Adrian Ko <adrianchko@gmail.com>
2015-01-26 09:32:29 +0000

    Basic news runner

---
+++ src/memento/News.java
@@ -0,0 +1,12 @@
+package memento;
+
+public class News {
+    
+    public static void main(String[] args) {
+        int currArticle, saveFiles = 0;
+        Caretaker c = new Caretaker();
+        Originator o = new Originator();
+        
+    }
+    
+}
