6ffc91e28cf2f3c27272c17a818487a58e4160ae
Adrian Ko <adrianchko@gmail.com>
2015-01-26 09:27:42 +0000

    Caretaker to add and get stored mementos

---
+++ src/memento/Caretaker.java
@@ -0,0 +1,17 @@
+package memento;
+
+import java.util.ArrayList;
+
+public class Caretaker {
+    
+    ArrayList<Memento> saved = new ArrayList<>();
+    
+    public void add(Memento m) {
+        saved.add(m);
+    }
+    
+    public Memento get(int i) {
+        return saved.get(i);
+    }
+    
+}
