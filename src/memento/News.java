dad13ecfd20bffaa7a7a3991cf021884a0314112
Adrian Ko <adrianchko@gmail.com>
2015-01-26 09:40:05 +0000

    Moving up and down the stored mementos

--- src/memento/News.java
+++ src/memento/News.java
@@ -3,14 +3,26 @@ package memento;
 public class News {
     
     public static void main(String[] args) {
-        int currArticle = 0, saveFiles = 0;
+        int currArticle = 0;
         Caretaker c = new Caretaker();
         Originator o = new Originator();
         
         o.set("Some text");
         c.add(o.store());
         currArticle++;
-        saveFiles++;
+
+        o.set("Some more text");
+        c.add(o.store());
+        currArticle++;
+
+        o.set("Even more text");
+        c.add(o.store());
+        currArticle++;
+        o.restore(c.get(--currArticle));
+        o.restore(c.get(--currArticle));
+        o.restore(c.get(--currArticle));
+        currArticle += 2;
+        o.restore(c.get(currArticle));
     }
     
 }
