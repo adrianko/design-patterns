1f657c7ca83eb995c1742743808b35f9851b7060
Adrian Ko <adrianchko@gmail.com>
2015-01-27 14:45:06 +0000

    Real Customer

---
+++ src/nullobject/RealCustomer.java
@@ -0,0 +1,19 @@
+package nullobject;
+
+public class RealCustomer extends AbstractCustomer {
+    
+    public RealCustomer(String n) {
+        name = n;
+    }
+    
+    @Override
+    public boolean isNil() {
+        return false;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+    
+}
