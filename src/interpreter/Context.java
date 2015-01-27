f5e53ebf89711d13086349a5d1af980291968764
Adrian Ko <adrianchko@gmail.com>
2015-01-27 08:00:10 +0000

    Add context for conversion

---
+++ src/interpreter/Context.java
@@ -0,0 +1,56 @@
+package interpreter;
+
+public class Context {
+    
+    private String question = "";
+    private String response = "";
+    private String from = "";
+    private String to = "";
+    private double quantity;
+    
+    String[] parts;
+    
+    public Context(String in) {
+        question = in;
+        parts = getInput().split(" ");
+        from = getCapitalised(parts[1]);
+        to = getLowercase(parts[3]);
+        quantity = Double.valueOf(parts[0]);
+        response = parts[0] + " " + parts[1] + " equals ";
+    }
+
+    public String getInput() {
+        return question;
+    }
+    
+    public String getFrom() {
+        return from;
+    }
+    
+    public String getTo() {
+        return to;
+    }
+    
+    public String getResponse() {
+        return response;
+    }
+    
+    public double getQuantity() {
+        return quantity;
+    }
+    
+    public String getLowercase(String w) {
+        return w.toLowerCase();    
+    }
+    
+    public String getCapitalised(String w) {
+        String wc = Character.toUpperCase(w.charAt(0)) + w.toLowerCase().substring(1);
+        
+        if (w.charAt(w.length() - 1) != 's') {
+            wc = new StringBuffer(wc).insert(wc.length(), "s").toString();
+        }
+        
+        return wc;
+    }
+    
+}
