7ebd97aaab0467cca8fd9730181131cff585204f
Adrian Ko <adrianchko@gmail.com>
2015-01-27 07:40:51 +0000

    Interpreter pattern

---
+++ src/interpreter/Conversion.java
@@ -0,0 +1,13 @@
+package interpreter;
+
+public class Conversion {
+
+    /**
+     * Leveraging reflection to extend functionality
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
