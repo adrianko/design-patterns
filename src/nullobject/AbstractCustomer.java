02b78a21ce4b6c398c4fd8b657db116979188302
Adrian Ko <adrianchko@gmail.com>
2015-01-27 14:44:28 +0000

    Separate properties and methods

--- src/nullobject/AbstractCustomer.java
+++ src/nullobject/AbstractCustomer.java
@@ -3,6 +3,7 @@ package nullobject;
 public abstract class AbstractCustomer {
     
     protected String name;
+    
     public abstract boolean isNil();
     public abstract String getName();
     
