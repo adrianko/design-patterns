fe71c51794255f5bd18678b917f86ecc31168354
AK <adrianchko@gmail.com>
2015-01-29 21:38:41 +0000

    Filter pattern, class to be filtered

---
+++ src/filter/Person.java
@@ -0,0 +1,27 @@
+package filter;
+
+public class Person {
+
+    private String name;
+    private String gender;
+    private String martialStatus;
+
+    public Person(String nm, String gen, String ms) {
+        name = nm;
+        gender = gen;
+        martialStatus = ms;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getMartialStatus() {
+        return martialStatus;
+    }
+    
+}
