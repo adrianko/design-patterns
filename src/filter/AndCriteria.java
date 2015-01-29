087cd751c448344b8fa23ddfcaecf34b9736534d
AK <adrianchko@gmail.com>
2015-01-29 21:55:47 +0000

    and criteria

--- src/filter/AndCriteria.java
+++ src/filter/AndCriteria.java
@@ -1,4 +1,20 @@
 package filter;
 
-public class AndCriteria {
+import java.util.List;
+
+public class AndCriteria implements Criteria {
+
+    private Criteria criteria;
+    private Criteria otherCriteria;
+
+    public AndCriteria(Criteria c, Criteria o) {
+        criteria = c;
+        otherCriteria = o;
+    }
+
+    @Override
+    public List<Person> meetCriteria(List<Person> persons) {
+        return otherCriteria.meetCriteria(criteria.meetCriteria(persons));
+    }
+
 }
