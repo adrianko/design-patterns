ab01697244fe83d44b77973e90cd1906b9a6e767
AK <adrianchko@gmail.com>
2015-01-29 21:45:33 +0000

    add female criteria

--- src/filter/CriteriaFemale.java
+++ src/filter/CriteriaFemale.java
@@ -1,4 +1,21 @@
 package filter;
 
-public class CriteriaFemale {
+import java.util.ArrayList;
+import java.util.List;
+
+public class CriteriaFemale implements Criteria {
+
+    @Override
+    public List<Person> meetCriteria(List<Person> persons) {
+        List<Person> female = new ArrayList<>();
+
+        for (Person p : persons) {
+            if (p.getGender().equalsIgnoreCase("FEMALE")) {
+                female.add(p);
+            }
+        }
+
+        return female;
+    }
+
 }
