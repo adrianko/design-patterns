f58cb965810dbb37f040b3ab31828db77d3d0217
AK <adrianchko@gmail.com>
2015-01-29 21:53:13 +0000

    remove extra bracket

--- src/filter/CriteriaSingle.java
+++ src/filter/CriteriaSingle.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CriteriaSingle implements Criteria {
-    
+
     @Override
     public List<Person> meetCriteria(List<Person> persons) {
         List<Person> single = new ArrayList<>();
 
         persons.forEach( p -> {
-           if (p.getMartialStatus().equalsIgnoreCase("SINGLE"))) {
+           if (p.getMartialStatus().equalsIgnoreCase("SINGLE")) {
                 single.add(p);
             }
         });
