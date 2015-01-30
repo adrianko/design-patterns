ffc8ae0d48f290c6f32938d12d0a3bbc5375cb56
AK <adrianchko@gmail.com>
2015-01-30 08:52:25 +0000

    add private file loader

--- src/proxy/RealImage.java
+++ src/proxy/RealImage.java
@@ -6,11 +6,16 @@ public class RealImage implements Image {
 
     public RealImage(String fn) {
         fileName = fn;
+        loadFileFromDisk(fileName);
     }
 
     @Override
     public void display() {
         System.out.println("Displaying " + fileName);
+    }
+
+    private void loadFileFromDisk(String fn) {
+        System.out.println("Loading " + fn);
     }
 
 }
