badcfca3b849003bb8fbffd5e21a961ec26362f2
AK <adrianchko@gmail.com>
2015-01-30 08:59:36 +0000

    proxy pattern explanation

--- src/proxy/Loader.java
+++ src/proxy/Loader.java
@@ -2,6 +2,12 @@ package proxy;
 
 public class Loader {
 
+    /**
+     * The proxy loads the real image and caches it, therefore when the image
+     * is called again, it doesn't have to be recalled from disk. The cached
+     * version is returned.
+     * @param args
+     */
     public static void main(String[] args) {
         Image im = new ProxyImage("test.jpg");
         im.display();
