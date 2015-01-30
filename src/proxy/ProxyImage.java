28d45f05c54ea652ee79cd57b9f036cbc2acaa8c
AK <adrianchko@gmail.com>
2015-01-30 08:55:33 +0000

    read in filename

--- src/proxy/ProxyImage.java
+++ src/proxy/ProxyImage.java
@@ -2,9 +2,16 @@ package proxy;
 
 public class ProxyImage implements Image {
 
+    private RealImage realImage;
+    private String fileName;
+
+    public ProxyImage(String fn) {
+        fileName = fn;
+    }
+
     @Override
     public void display() {
-               
+
     }
 
 }
