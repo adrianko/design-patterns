3a4b17e7fd371bac61b6839f7a06d9cd203a42e9
AK <adrianchko@gmail.com>
2015-01-30 08:51:27 +0000

    read in filename

--- src/proxy/RealImage.java
+++ src/proxy/RealImage.java
@@ -2,9 +2,15 @@ package proxy;
 
 public class RealImage implements Image {
 
+    private String fileName;
+
+    public RealImage(String fn) {
+        fileName = fn;
+    }
+
     @Override
     public void display() {
-
+        System.out.println("Displaying " + fileName);
     }
 
 }
