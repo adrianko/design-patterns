653fda6b27c40bc937d80d93d84e452ff02dfc1a
AK <adrianchko@gmail.com>
2015-01-31 15:48:34 +0000

    add method

--- src/businessdelegate/BusinessLookUp.java
+++ src/businessdelegate/BusinessLookUp.java
@@ -1,4 +1,12 @@
 package businessdelegate;
 
 public class BusinessLookUp {
+
+    public BusinessService getBusinessService(String st) {
+        if (st.equalsIgnoreCase("EJB")) {
+            return new EJBService();
+        }
+        return new JMSService();
+    }
+
 }
