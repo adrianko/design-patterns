45136ce878be152c162eb07096a9c1dfa3951838
AK <adrianchko@gmail.com>
2015-02-01 12:34:43 +0000

    add first dependent object for composite entity pattern

---
+++ src/compositeentity/DependentObject1.java
@@ -0,0 +1,15 @@
+package compositeentity;
+
+public class DependentObject1 {
+
+    private String data;
+
+    public void setData(String d) {
+        data = d;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+}
