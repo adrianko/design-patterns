e4d4c687407421702d5d93a682db649ad060c38d
AK <adrianchko@gmail.com>
2015-02-01 12:40:53 +0000

    add client

---
+++ src/compositeentity/Client.java
@@ -0,0 +1,17 @@
+package compositeentity;
+
+public class Client {
+
+    private CompositeEntity ce = new CompositeEntity();
+
+    public void printData() {
+        for (String d : ce.getData()) {
+            System.out.println("Data: " + d);
+        }
+    }
+
+    public void setData(String d1, String d2) {
+        ce.setData(d1, d2);
+    }
+
+}
