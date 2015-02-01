64f23e23a777a5e24a8277027335992d5c05f064
AK <adrianchko@gmail.com>
2015-02-01 12:35:47 +0000

    add another dependent object

---
+++ src/compositeentity/DependentObject2.java
@@ -0,0 +1,15 @@
+package compositeentity;
+
+public class DependentObject2 {
+
+    private String data;
+
+    public void setData(String d) {
+        data = d;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+}
