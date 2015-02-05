ffb088d069b9696d2b86de0db4263937719caa5d
Adrian Ko <adrianchko@gmail.com>
2015-02-05 15:51:07 +0000

    FilterManager constructor

--- src/interceptingfilter/FilterManager.java
+++ src/interceptingfilter/FilterManager.java
@@ -1,4 +1,12 @@
 package interceptingfilter;
 
 public class FilterManager {
+    
+    FilterChain filterChain;
+    
+    public FilterManager(Target t) {
+        filterChain = new FilterChain();
+        filterChain.setTarget(t);
+    }
+    
 }
