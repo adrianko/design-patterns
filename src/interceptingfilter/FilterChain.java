52d74cf8bbd7396b6911faad570acb293e4ee3d1
Adrian Ko <adrianchko@gmail.com>
2015-02-05 15:49:26 +0000

    Add filter chain methods

--- src/interceptingfilter/FilterChain.java
+++ src/interceptingfilter/FilterChain.java
@@ -8,4 +8,20 @@ public class FilterChain {
     private List<Filter> filters = new ArrayList<>();
     private Target target;
     
+    public void addFilter(Filter f) {
+        filters.add(f);
+    }
+    
+    public void execute(String request) {
+        for (Filter f : filters) {
+            f.execute(request);
+        }
+        
+        target.execute(request);
+    }
+    
+    public void setTarget(Target t) {
+        target = t;
+    }
+    
 }
