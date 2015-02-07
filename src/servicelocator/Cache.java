78c2c017e05cd4bd6cc6acd7e44ff9dfce14a213
AK <adrianchko@gmail.com>
2015-02-07 12:32:09 +0000

    Create Member and initialize in Cache

--- src/servicelocator/Cache.java
+++ src/servicelocator/Cache.java
@@ -1,4 +1,14 @@
 package servicelocator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cache {
+
+    private List<Service> services;
+
+    public Cache() {
+        services = new ArrayList<>();
+    }
+
 }
