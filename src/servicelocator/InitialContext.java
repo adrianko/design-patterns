041c44c3fb41d2990490078fa6be9f475afd8b37
AK <adrianchko@gmail.com>
2015-02-07 12:30:46 +0000

    Create InitialContext

---
+++ src/servicelocator/InitialContext.java
@@ -0,0 +1,19 @@
+package servicelocator;
+
+public class InitialContext {
+
+    public Object lookup(String jndiName) {
+        if (jndiName.equalsIgnoreCase("SERVICEONE")) {
+            System.out.println("Looking up and creating a new ServiceOne object");
+
+            return new ServiceOne();
+        } else if (jndiName.equalsIgnoreCase("SERVICETWO")) {
+            System.out.println("Looking up and creating a new ServiceTwo object");
+
+            return new ServiceTwo();
+        }
+
+        return null;
+    }
+
+}
