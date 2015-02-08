752432e5825018f18ce3b5c21c26a8fecaaf1ec4
AK <adrianchko@gmail.com>
2015-02-08 13:55:23 +0000

    Add Blue class implementing Colour

---
+++ src/abstractfactory/shape/Blue.java
@@ -0,0 +1,10 @@
+package abstractfactory.shape;
+
+public class Blue implements Colour {
+
+    @Override
+    public void fill() {
+        System.out.println("Inside Blue::fill() method.");
+    }
+
+}
