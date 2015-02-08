e14d9a6aac6fb75110604ff4dcb2be53af8074fd
AK <adrianchko@gmail.com>
2015-02-08 13:59:35 +0000

    Create ShapeFactory and override method

--- src/abstractfactory/shape/ShapeFactory.java
+++ src/abstractfactory/shape/ShapeFactory.java
@@ -1,4 +1,23 @@
 package abstractfactory.shape;
 
-public class ShapeFactory {
+public class ShapeFactory extends AbstractFactory {
+
+    @Override
+    Colour getColour(String colour) {
+        return null;
+    }
+
+    @Override
+    Shape getShape(String shape) {
+        if (shape.equalsIgnoreCase("CIRCLE")) {
+            return new Circle();
+        } else if (shape.equalsIgnoreCase("RECTANGLE")) {
+            return new Rectangle();
+        } else if (shape.equalsIgnoreCase("SQUARE")) {
+            return new Square();
+        }
+        
+        return null;
+    }
+
 }
