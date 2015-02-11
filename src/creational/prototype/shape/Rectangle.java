9147b834a5dffe200586ca72a3e9b892ae42b85c
AK <adrianchko@gmail.com>
2015-02-11 22:01:26 +0000

    Square concrete of Shape

--- src/creational/prototype/shape/Rectangle.java
+++ src/creational/prototype/shape/Rectangle.java
@@ -9,5 +9,5 @@ public class Rectangle extends Shape {
     public void draw() {
         System.out.println("Rectangle::draw()");
     }
-    
+
 }
---
+++ src/creational/prototype/shape/Square.java
@@ -0,0 +1,13 @@
+package creational.prototype.shape;
+
+public class Square extends Shape {
+
+    public Square() {
+        type = "Square";
+    }
+
+    public void draw() {
+        System.out.println("Square::draw()");
+    }
+    
+}
