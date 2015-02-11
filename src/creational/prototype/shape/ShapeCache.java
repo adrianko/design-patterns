eb0d489de81ca15d14b1f5ddaa8f3e2ab7f256cd
AK <adrianchko@gmail.com>
2015-02-11 22:03:57 +0000

    Add getShape method

--- src/creational/prototype/shape/ShapeCache.java
+++ src/creational/prototype/shape/ShapeCache.java
@@ -6,4 +6,10 @@ public class ShapeCache {
 
     private static Hashtable<String, Shape> shapeMap = new Hashtable<String, Shape>();
 
+    public static Shape getShape(String shapeID) {
+        Shape cachedShape = shapeMap.get(shapeID);
+
+        return (Shape) cachedShape.clone();
+    }
+
 }
