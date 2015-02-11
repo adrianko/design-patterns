51b5dbad59b85457d842bb366d3cedc3322182ed
Adrian Ko <adrianchko@gmail.com>
2015-02-11 09:43:13 +0000

    Create getCircle method

--- src/structural/flyweight/shape/ShapeFactory.java
+++ src/structural/flyweight/shape/ShapeFactory.java
@@ -4,5 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ShapeFactory {
+    
     private static final Map<String, Shape> circles = new HashMap<>();
+    
+    public static Shape getCircle(String colour) {
+        Circle c = (Circle) circles.get(colour);    
+        
+        if (c == null) {
+            c = new Circle(colour);
+            circles.put(colour, c);
+            System.out.println("Creating circle of colour: " + colour);
+        }
+        
+        return c;
+    }
+    
 }
