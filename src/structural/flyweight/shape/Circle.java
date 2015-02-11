e0754b85582bf354543d41d741633c06d16c0ae4
Adrian Ko <adrianchko@gmail.com>
2015-02-11 09:32:26 +0000

    Create private members

--- src/structural/flyweight/shape/Circle.java
+++ src/structural/flyweight/shape/Circle.java
@@ -2,6 +2,11 @@ package structural.flyweight.shape;
 
 public class Circle implements Shape {
     
+    private String colour;
+    private int x;
+    private int y;
+    private int radius;
+    
     @Override
     public void draw() {
         
