8aae722355bd1f7197340e9ae1c43707f052969e
AK <adrianchko@gmail.com>
2015-02-11 22:08:22 +0000

    Get final from cache

--- src/creational/prototype/shape/ShapeCache.java
+++ src/creational/prototype/shape/ShapeCache.java
@@ -34,6 +34,9 @@ public class ShapeCache {
 
         Shape clonedShape2 = ShapeCache.getShape("2");
         System.out.println("Shape: " + clonedShape2.getType());
+
+        Shape clonedShape3 = ShapeCache.getShape("3");
+        System.out.println("Shape: " + clonedShape3.getType());
     }
 
 }
