2c18958300b45da15b1a2a17d3aed965d9d3303e
AK <adrianchko@gmail.com>
2015-02-11 22:03:01 +0000

    Create ShapeCache and add member

---
+++ src/creational/prototype/shape/ShapeCache.java
@@ -0,0 +1,9 @@
+package creational.prototype.shape;
+
+import java.util.Hashtable;
+
+public class ShapeCache {
+
+    private static Hashtable<String, Shape> shapeMap = new Hashtable<String, Shape>();
+
+}
