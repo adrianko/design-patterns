9288f933effce021d52be3b6f3c4b321e3666e4f
AK <adrianchko@gmail.com>
2015-02-11 22:00:35 +0000

    Rectangle concrete of Shape

---
+++ src/creational/prototype/shape/Rectangle.java
@@ -0,0 +1,13 @@
+package creational.prototype.shape;
+
+public class Rectangle extends Shape {
+
+    public Rectangle() {
+        type = "Rectangle";
+    }
+
+    public void draw() {
+        System.out.println("Rectangle::draw()");
+    }
+    
+}
