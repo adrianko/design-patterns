3d7f54c2d8656834f800fbf98f5b3ebed0fd800d
Adrian Ko <adrianchko@gmail.com>
2015-02-11 09:40:10 +0000

    Add circle stashing

--- src/structural/flyweight/shape/ShapeFactory.java
+++ src/structural/flyweight/shape/ShapeFactory.java
@@ -1,4 +1,8 @@
 package structural.flyweight.shape;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ShapeFactory {
+    private static final Map<String, Shape> circles = new HashMap<>();
 }
