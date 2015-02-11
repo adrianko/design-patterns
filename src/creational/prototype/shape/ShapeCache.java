70ae9b9d94c061c349727c3badf77c7a8c45f895
AK <adrianchko@gmail.com>
2015-02-11 22:08:02 +0000

    Get second from cache

--- src/creational/prototype/shape/ShapeCache.java
+++ src/creational/prototype/shape/ShapeCache.java
@@ -31,6 +31,9 @@ public class ShapeCache {
 
         Shape clonedShape = ShapeCache.getShape("1");
         System.out.println("Shape: " + clonedShape.getType());
+
+        Shape clonedShape2 = ShapeCache.getShape("2");
+        System.out.println("Shape: " + clonedShape2.getType());
     }
 
 }
