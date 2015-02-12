9a2613d2bb1c289d70d03cf4326a3a4aaa4dc1de
Adrian Ko <adrianchko@gmail.com>
2015-02-12 09:23:59 +0000

    Add base employee groups

--- src/structural/composite/employee/HR.java
+++ src/structural/composite/employee/HR.java
@@ -3,7 +3,9 @@ package structural.composite.employee;
 public class HR {
     
     public static void main(String[] args) {
-        
+        Employee ceo = new Employee("John", "CEO", 30000);
+        Employee headSales = new Employee("Robert", "Head of Sales", 20000);
+        Employee headMkt = new Employee("Michael", "Head or Marketing", 20000);
     }
     
 }
