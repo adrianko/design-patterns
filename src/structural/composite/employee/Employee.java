28ae7f61d424ad1e3f46606bfc61e09869abb3b9
Adrian Ko <adrianchko@gmail.com>
2015-02-12 09:17:22 +0000

    Add members of class

--- src/structural/composite/employee/Employee.java
+++ src/structural/composite/employee/Employee.java
@@ -1,4 +1,12 @@
 package structural.composite.employee;
 
+import java.util.List;
+
 public class Employee {
+    
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+    
 }
