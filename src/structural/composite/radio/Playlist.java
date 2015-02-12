207b4432e26d5b71f55be8f86c4d6de68970928e
Adrian Ko <adrianchko@gmail.com>
2015-02-12 09:15:54 +0000

    Move DJ into Radio package

--- src/structural/composite/radio/Playlist.java
+++ src/structural/composite/radio/Playlist.java
@@ -1,4 +1,4 @@
-package structural.composite;
+package structural.composite.radio;
 
 public class Playlist {
 
--- src/structural/composite/radio/SongGroup.java
+++ src/structural/composite/radio/SongGroup.java
@@ -1,4 +1,4 @@
-package structural.composite;
+package structural.composite.radio;
 
 import java.util.ArrayList;
 
