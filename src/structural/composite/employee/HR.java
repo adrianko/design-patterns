819587290105d7dd20d38d201b883d8ba5faecba
Adrian Ko <adrianchko@gmail.com>
2015-02-12 09:30:52 +0000

    lazy initialisation

--- src/structural/composite/employee/HR.java
+++ src/structural/composite/employee/HR.java
@@ -12,20 +12,14 @@ public class HR {
         Employee headSales = new Employee("Robert", "Head of Sales", 20000);
         Employee headMkt = new Employee("Michael", "Head of Marketing", 20000);
 
-        Employee clerk1 = new Employee("Laura", "Marketing", 10000);
-        Employee clerk2 = new Employee("Bob", "Marketing", 10000);
-
-        Employee salesExecutive1 = new Employee("Richard", "Sales", 10000);
-        Employee salesExecutive2 = new Employee("Rob", "Sales", 10000);
-        
         ceo.add(headSales);
         ceo.add(headMkt);
         
-        headSales.add(salesExecutive1);
-        headSales.add(salesExecutive2);
+        headSales.add(new Employee("Richard", "Sales", 10000));
+        headSales.add(new Employee("Rob", "Sales", 10000));
         
-        headMkt.add(clerk1);
-        headMkt.add(clerk2);
+        headMkt.add(new Employee("Laura", "Marketing", 10000));
+        headMkt.add(new Employee("Bob", "Marketing", 10000));
 
         System.out.println(ceo);
         
