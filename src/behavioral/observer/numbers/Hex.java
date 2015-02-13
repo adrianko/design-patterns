3c3ac83094783bc3c3563ce7f61347347e909702
Adrian Ko <adrianchko@gmail.com>
2015-02-13 09:45:28 +0000

    Override Hex

--- src/behavioral/observer/numbers/Hex.java
+++ src/behavioral/observer/numbers/Hex.java
@@ -1,4 +1,10 @@
 package behavioral.observer.numbers;
 
-public class Hex {
+public class Hex extends Observer {
+
+    @Override
+    public void update() {
+        System.out.println("Hex String: " + Integer.toHexString(subject.getState()).toUpperCase());
+    }
+    
 }
