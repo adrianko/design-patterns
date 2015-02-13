bc7c56a7e3e7ce120adbcc94a0291b060fdc2d93
Adrian Ko <adrianchko@gmail.com>
2015-02-13 09:36:31 +0000

    Add members

--- src/behavioral/observer/numbers/Subject.java
+++ src/behavioral/observer/numbers/Subject.java
@@ -1,4 +1,11 @@
 package behavioral.observer.numbers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Subject {
+    
+    private List<Observer> observers = new ArrayList<>();
+    private int state;
+    
 }
