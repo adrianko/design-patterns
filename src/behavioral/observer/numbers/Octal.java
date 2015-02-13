50749c1dc12a3783ab00478a622e3807f4cdacee
Adrian Ko <adrianchko@gmail.com>
2015-02-13 09:44:08 +0000

    Create constructor

--- src/behavioral/observer/numbers/Octal.java
+++ src/behavioral/observer/numbers/Octal.java
@@ -2,6 +2,11 @@ package behavioral.observer.numbers;
 
 public class Octal extends Observer {
     
+    public Octal(Subject s) {
+        subject = s;
+        subject.attach(this);
+    }
+    
     @Override
     public void update() {
         System.out.println("Octal String: " + Integer.toOctalString(subject.getState()));
