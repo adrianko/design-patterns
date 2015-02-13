f3ad6430091b3d7ea37649efed032dd2f5b74931
Adrian Ko <adrianchko@gmail.com>
2015-02-13 09:41:58 +0000

    Override update method

--- src/behavioral/observer/numbers/Binary.java
+++ src/behavioral/observer/numbers/Binary.java
@@ -4,7 +4,7 @@ public class Binary extends Observer {
     
     @Override
     public void update() {
-        
+        System.out.println("Binary String: " + Integer.toBinaryString(subject.getState()));
     }
     
 }
