f2a0ae4693ede11dba01c0bc66b6b8c6f70b60dc
Adrian Ko <adrianchko@gmail.com>
2015-02-13 09:47:51 +0000

    Add main content

--- src/behavioral/observer/numbers/Conversion.java
+++ src/behavioral/observer/numbers/Conversion.java
@@ -3,7 +3,17 @@ package behavioral.observer.numbers;
 public class Conversion {
     
     public static void main(String[] args) {
+        Subject s = new Subject();
         
+        new Binary(s);
+        new Octal(s);
+        new Hex(s);
+
+        System.out.println("First state change: 15");
+        s.setState(15);
+
+        System.out.println("Second state change: 10");
+        s.setState(10);
     }
     
 }
