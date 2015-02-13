36afc900bf687892a53c5bb0fc001ff15f922fef
Adrian Ko <adrianchko@gmail.com>
2015-02-13 09:42:33 +0000

    Create constructor to attach self to subject

--- src/behavioral/observer/numbers/Binary.java
+++ src/behavioral/observer/numbers/Binary.java
@@ -2,6 +2,11 @@ package behavioral.observer.numbers;
 
 public class Binary extends Observer {
     
+    public Binary(Subject s) {
+        subject = s;
+        subject.attach(this);
+    } 
+    
     @Override
     public void update() {
         System.out.println("Binary String: " + Integer.toBinaryString(subject.getState()));
