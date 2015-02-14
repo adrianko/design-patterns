a1421167038dfb8982da3cb4bdab5057c255974e
AK <adrianchko@gmail.com>
2015-02-14 20:44:17 +0000

    Add explanation of Mediator pattern

--- src/behavioral/mediator/chatroom/Website.java
+++ src/behavioral/mediator/chatroom/Website.java
@@ -2,6 +2,12 @@ package behavioral.mediator.chatroom;
 
 public class Website {
 
+    /**
+     * Mediator - used to reduce communication complexity between multiple objects or classes. Provides a mediator class
+     * which normally handles all the communications between different classes and supports easy maintenance of the code
+     * by loose coupling.
+     * @param args
+     */
     public static void main(String[] args) {
         User ak = new User("AK");
         User jj = new User("JJ");
