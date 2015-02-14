d6c6efb72a88840790eaf327da896f6f744924a8
AK <adrianchko@gmail.com>
2015-02-14 20:38:05 +0000

    Create method body

--- src/behavioral/mediator/chatroom/ChatRoom.java
+++ src/behavioral/mediator/chatroom/ChatRoom.java
@@ -1,9 +1,11 @@
 package behavioral.mediator.chatroom;
 
+import java.util.Date;
+
 public class ChatRoom {
 
-    public static void showMessage(User u, String m) {
-
+    public static void showMessage(User user, String message) {
+        System.out.println(new Date().toString() + " [" + user.getName() + "] : " + message);
     }
 
 }
