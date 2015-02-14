2e6414e6923e32aab752a3d5bb66ce5b5afbd6a7
AK <adrianchko@gmail.com>
2015-02-14 20:42:08 +0000

    Create users

--- src/behavioral/mediator/chatroom/Website.java
+++ src/behavioral/mediator/chatroom/Website.java
@@ -3,7 +3,8 @@ package behavioral.mediator.chatroom;
 public class Website {
 
     public static void main(String[] args) {
-
+        User ak = new User("AK");
+        User jj = new User("JJ");
     }
 
 }
