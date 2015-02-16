b834c95d20fa21063a941bc9b4dc9e9a668986c9
AK <adrianchko@gmail.com>
2015-02-16 06:31:12 +0000

    Override write and read in level

--- src/behavioral/chainofresponsibility/logger/Console.java
+++ src/behavioral/chainofresponsibility/logger/Console.java
@@ -1,4 +1,14 @@
 package behavioral.chainofresponsibility.logger;
 
-public class Console {
+public class Console extends AbstractLogger {
+
+    public Console(int l) {
+        level = l;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Standard Console::Logger: " + message);
+    }
+
 }
