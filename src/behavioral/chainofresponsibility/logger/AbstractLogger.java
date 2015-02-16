49ed051bd6ebf85b8e765b07b7fd84318a2fe5dd
AK <adrianchko@gmail.com>
2015-02-16 06:25:28 +0000

    Create members

--- src/behavioral/chainofresponsibility/logger/AbstractLogger.java
+++ src/behavioral/chainofresponsibility/logger/AbstractLogger.java
@@ -1,4 +1,9 @@
 package behavioral.chainofresponsibility.logger;
 
-public class AbstractLogger {
+public abstract class AbstractLogger {
+
+    public static int INFO = 1;
+    public static int DEBUG = 2;
+    public static int ERROR = 3;
+
 }
