52e0a58610db8b767102c0b7fa188ec06a95ef22
AK <adrianchko@gmail.com>
2015-02-16 06:32:44 +0000

    Error overrides

--- src/behavioral/chainofresponsibility/logger/Error.java
+++ src/behavioral/chainofresponsibility/logger/Error.java
@@ -1,4 +1,14 @@
 package behavioral.chainofresponsibility.logger;
 
-public class Error {
+public class Error extends AbstractLogger {
+
+    public Error(int i) {
+        level = i;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("Error Console::Logger: " + message);
+    }
+
 }
