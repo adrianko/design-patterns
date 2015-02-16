0c15eaceb84044f5e4209f0cd122eeb7793c58b7
AK <adrianchko@gmail.com>
2015-02-16 06:38:36 +0000

    Create loggers

--- src/behavioral/chainofresponsibility/logger/Logger.java
+++ src/behavioral/chainofresponsibility/logger/Logger.java
@@ -3,7 +3,9 @@ package behavioral.chainofresponsibility.logger;
 public class Logger {
 
     private static AbstractLogger getLogChain() {
-
+        AbstractLogger err = new Error(AbstractLogger.ERROR);
+        AbstractLogger file = new File(AbstractLogger.DEBUG);
+        AbstractLogger console = new Console(AbstractLogger.INFO);
     }
 
     public static void main(String[] args) {
