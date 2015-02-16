93acc42f4b8cf668c0973fbde53cb02d007f7748
AK <adrianchko@gmail.com>
2015-02-16 06:39:30 +0000

    Create chain

--- src/behavioral/chainofresponsibility/logger/Logger.java
+++ src/behavioral/chainofresponsibility/logger/Logger.java
@@ -6,6 +6,11 @@ public class Logger {
         AbstractLogger err = new Error(AbstractLogger.ERROR);
         AbstractLogger file = new File(AbstractLogger.DEBUG);
         AbstractLogger console = new Console(AbstractLogger.INFO);
+
+        err.setNext(file);
+        file.setNext(console);
+
+        return err;
     }
 
     public static void main(String[] args) {
