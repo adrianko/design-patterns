2e228e85ca2ca8277cd1bcd6b9cac140e90e7574
AK <adrianchko@gmail.com>
2015-02-16 06:35:17 +0000

    File overrides

--- src/behavioral/chainofresponsibility/logger/File.java
+++ src/behavioral/chainofresponsibility/logger/File.java
@@ -1,4 +1,13 @@
 package behavioral.chainofresponsibility.logger;
 
-public class File {
+public class File extends AbstractLogger {
+
+    public File(int l) {
+        level = l;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("File::Logger: " + message);
+    }
 }
