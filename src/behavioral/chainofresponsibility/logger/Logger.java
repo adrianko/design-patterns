e6a7a8c734ebe1515ed7f01f1fe169fbb484d65a
AK <adrianchko@gmail.com>
2015-02-16 06:37:05 +0000

    Create main and getLogChain methods

--- src/behavioral/chainofresponsibility/logger/Logger.java
+++ src/behavioral/chainofresponsibility/logger/Logger.java
@@ -1,4 +1,13 @@
 package behavioral.chainofresponsibility.logger;
 
 public class Logger {
+
+    private static AbstractLogger getLogChain() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
