194adf7217b1fdc1d4c2bdd5b9dd6011236ffc3a
Adrian Ko <adrianchko@gmail.com>
2015-02-17 07:56:12 +0000

    Draw shapes

--- src/structural/decorator/shape/ShapeGenerator.java
+++ src/structural/decorator/shape/ShapeGenerator.java
@@ -6,6 +6,15 @@ public class ShapeGenerator {
         Shape circle = new Circle();
         Shape redCircle = new RedShapeDecorator(new Circle());
         Shape redRectangle = new RedShapeDecorator(new Rectangle());
+
+        System.out.println("Circle with normal border: ");
+        circle.draw();
+
+        System.out.println("\nCircle with red border: ");
+        redCircle.draw();
+
+        System.out.println("\nRectangle with red border: ");
+        redRectangle.draw();
     }
     
 }
