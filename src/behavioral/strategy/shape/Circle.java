5798e9b57728c7c8c376ae552223aba17cc815bb
Adrian Ko <adrianchko@gmail.com>
2015-02-17 07:47:47 +0000

    Add Circle implementing Shape

---
+++ src/behavioral/strategy/shape/Circle.java
@@ -0,0 +1,10 @@
+package behavioral.strategy.shape;
+
+public class Circle implements Shape {
+    
+    @Override
+    public void draw() {
+        System.out.println("Shape: Circle");
+    }
+    
+}
