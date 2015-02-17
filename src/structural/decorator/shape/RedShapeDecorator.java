8826d063ed8edbdfb6b9694aad3fe9220c239b03
Adrian Ko <adrianchko@gmail.com>
2015-02-17 07:53:51 +0000

    Add override and red border coour

--- src/structural/decorator/shape/RedShapeDecorator.java
+++ src/structural/decorator/shape/RedShapeDecorator.java
@@ -1,4 +1,19 @@
 package structural.decorator.shape;
 
-public class RedShapeDecorator {
+public class RedShapeDecorator  extends ShapeDecorator {
+    
+    public RedShapeDecorator(Shape ds) {
+        super(ds);
+    }
+    
+    @Override
+    public void draw() {
+        decoratedShape.draw();
+        setRedBorder(decoratedShape);
+    }
+    
+    public void setRedBorder(Shape ds) {
+        System.out.println("Borer Colour: Red");
+    }
+    
 }
