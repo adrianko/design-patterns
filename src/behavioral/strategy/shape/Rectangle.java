d04ce2f42ef6dab3d7105ce5ea9e3415fdf0f59f
Adrian Ko <adrianchko@gmail.com>
2015-02-17 07:47:04 +0000

    Method override

--- src/behavioral/strategy/shape/Rectangle.java
+++ src/behavioral/strategy/shape/Rectangle.java
@@ -1,4 +1,10 @@
 package behavioral.strategy.shape;
 
-public class Rectangle {
+public class Rectangle implements Shape {
+    
+    @Override
+    public void draw() {
+        System.out.println("Shape: Rectangle");       
+    }
+    
 }
