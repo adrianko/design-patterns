672ad405f3f34ebd2f487840cd63e30640471d83
Adrian Ko <adrianchko@gmail.com>
2015-02-17 07:51:42 +0000

    Call stashed member draw method

--- src/structural/decorator/shape/ShapeDecorator.java
+++ src/structural/decorator/shape/ShapeDecorator.java
@@ -4,4 +4,9 @@ public abstract class ShapeDecorator implements Shape {
     
     protected Shape decoratedShape;
     
+    @Override
+    public void draw() {
+        decoratedShape.draw();
+    }
+    
 }
