a01426704c0fe5d80eea2b5f43228a55b1dc51b2
Adrian Ko <adrianchko@gmail.com>
2015-02-17 07:55:07 +0000

    Create basic shapes

---
+++ src/structural/decorator/shape/ShapeGenerator.java
@@ -0,0 +1,11 @@
+package structural.decorator.shape;
+
+public class ShapeGenerator {
+    
+    public static void main(String[] args) {
+        Shape circle = new Circle();
+        Shape redCircle = new RedShapeDecorator(new Circle());
+        Shape redRectangle = new RedShapeDecorator(new Rectangle());
+    }
+    
+}
