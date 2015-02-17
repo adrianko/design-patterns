4dce76b632c187104fbfe9b336493f097cd7b801
Adrian Ko <adrianchko@gmail.com>
2015-02-17 07:57:41 +0000

    Explanation of decorator pattern

--- src/structural/decorator/shape/ShapeGenerator.java
+++ src/structural/decorator/shape/ShapeGenerator.java
@@ -1,7 +1,13 @@
 package structural.decorator.shape;
 
 public class ShapeGenerator {
-    
+
+    /**
+     * Decorator - allows adding of new functionality to an existing object without altering its structure. The
+     * pattern creates a decorator class which wraps the original class and provides additional functionality
+     * keeping class methods signature intact.
+     * @param args
+     */
     public static void main(String[] args) {
         Shape circle = new Circle();
         Shape redCircle = new RedShapeDecorator(new Circle());
