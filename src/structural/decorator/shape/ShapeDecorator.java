073f3740dc7cb631a899ff8600941f25b425f0f1
Adrian Ko <adrianchko@gmail.com>
2015-02-17 07:51:48 +0000

    Read in shape

--- src/structural/decorator/shape/ShapeDecorator.java
+++ src/structural/decorator/shape/ShapeDecorator.java
@@ -4,6 +4,10 @@ public abstract class ShapeDecorator implements Shape {
     
     protected Shape decoratedShape;
     
+    public ShapeDecorator(Shape ds) {
+        decoratedShape = ds;
+    }
+    
     @Override
     public void draw() {
         decoratedShape.draw();
