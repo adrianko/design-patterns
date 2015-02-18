7939cf7d2883ad6e051bd9ac68df25f6d3628f88
AK <adrianchko@gmail.com>
2015-02-18 06:34:32 +0000

    Add explanation of Strategy

--- src/behavioral/strategy/calculator/Calculator.java
+++ src/behavioral/strategy/calculator/Calculator.java
@@ -2,6 +2,12 @@ package behavioral.strategy.calculator;
 
 public class Calculator {
 
+    /**
+     * Strategy - A class behaviour or its algorithm can be changed at runtime. Various operation objects represent strategies
+     * and a context object whose behaviour varies as per its strategy object. The strategy object changes the executing
+     * algorithm of the context object.
+     * @param args
+     */
     public static void main(String[] args) {
         Context c1 = new Context(new OperationAdd());
         System.out.println("10 + 5 = " + c1.executeStrategy(10, 5));
