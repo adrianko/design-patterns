286b5c698c16b077061bb4ff4e574e4199104e57
AK <adrianchko@gmail.com>
2015-02-18 06:31:09 +0000

    Create first usage

---
+++ src/behavioral/strategy/calculator/Calculator.java
@@ -0,0 +1,10 @@
+package behavioral.strategy.calculator;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Context c1 = new Context(new OperationAdd());
+        System.out.println("10 + 5 = " + c1.executeStrategy(10, 5));
+    }
+
+}
