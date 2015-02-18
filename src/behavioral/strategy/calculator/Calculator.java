fda4a2e93e62d53f21388f0c437f02f2128e4f05
AK <adrianchko@gmail.com>
2015-02-18 06:31:37 +0000

    Create second usage

--- src/behavioral/strategy/calculator/Calculator.java
+++ src/behavioral/strategy/calculator/Calculator.java
@@ -5,6 +5,9 @@ public class Calculator {
     public static void main(String[] args) {
         Context c1 = new Context(new OperationAdd());
         System.out.println("10 + 5 = " + c1.executeStrategy(10, 5));
+
+        Context c2 = new Context(new OperationSubtract());
+        System.out.println("10 - 5 = " + c2.executeStrategy(10, 5));
     }
 
 }
