f5e4b3bd833d38e409db743a21f636d002bd2a52
AK <adrianchko@gmail.com>
2015-02-18 06:26:25 +0000

    Create OperationSubtract

---
+++ src/behavioral/strategy/calculator/OperationSubtract.java
@@ -0,0 +1,10 @@
+package behavioral.strategy.calculator;
+
+public class OperationSubtract implements Strategy {
+
+    @Override
+    public int doOperation(int n1, int n2) {
+        return n1 - n2;
+    }
+
+}
