50560eacf732af47bd0bd8912380f3261a14a534
AK <adrianchko@gmail.com>
2015-02-19 06:24:47 +0000

    Create first expression

---
+++ src/behavioral/interpreter/expression/Interpret.java
@@ -0,0 +1,11 @@
+package behavioral.interpreter.expression;
+
+public class Interpret {
+
+    public static Expression getMaleExpression() {
+        Expression robert = new TerminalExpression("Robert");
+        Expression john = new TerminalExpression("John");
+        return new OrExpression(robert, john);
+    }
+
+}
