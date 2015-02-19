516afcd75945bd3ae4a9c608de287540b18af5ff
AK <adrianchko@gmail.com>
2015-02-19 06:17:35 +0000

    Create constructor

--- src/behavioral/interpreter/expression/TerminalExpression.java
+++ src/behavioral/interpreter/expression/TerminalExpression.java
@@ -4,6 +4,10 @@ public class TerminalExpression implements Expression {
 
     private String data;
 
+    public TerminalExpression(String d) {
+        data = d;
+    }
+
     @Override
     public boolean interpret(String context) {
         return false;
