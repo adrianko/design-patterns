fd17bdb3d1f0de9eea31eadcecb29bc34fd41560
AK <adrianchko@gmail.com>
2015-02-19 06:17:14 +0000

    Add member

--- src/behavioral/interpreter/expression/TerminalExpression.java
+++ src/behavioral/interpreter/expression/TerminalExpression.java
@@ -2,6 +2,8 @@ package behavioral.interpreter.expression;
 
 public class TerminalExpression implements Expression {
 
+    private String data;
+
     @Override
     public boolean interpret(String context) {
         return false;
