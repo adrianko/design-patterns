61c956a09292a301df82ad09eb0f524829cf12a5
AK <adrianchko@gmail.com>
2015-02-19 06:26:07 +0000

    Create second expression

--- src/behavioral/interpreter/expression/Interpret.java
+++ src/behavioral/interpreter/expression/Interpret.java
@@ -5,7 +5,15 @@ public class Interpret {
     public static Expression getMaleExpression() {
         Expression robert = new TerminalExpression("Robert");
         Expression john = new TerminalExpression("John");
+
         return new OrExpression(robert, john);
+    }
+
+    public static Expression getMarriedWomanExpression() {
+        Expression julie = new TerminalExpression("Julie");
+        Expression married = new TerminalExpression("Married");
+
+        return new AndExpression(julie, married);
     }
 
 }
