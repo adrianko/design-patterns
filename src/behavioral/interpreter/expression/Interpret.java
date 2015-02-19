3db49389b3a07cc362d3076af9cfbd4af4d97a01
AK <adrianchko@gmail.com>
2015-02-19 06:27:04 +0000

    Run expressions

--- src/behavioral/interpreter/expression/Interpret.java
+++ src/behavioral/interpreter/expression/Interpret.java
@@ -16,4 +16,12 @@ public class Interpret {
         return new AndExpression(julie, married);
     }
 
+    public static void main(String[] args) {
+        Expression isMale = getMaleExpression();
+        Expression isMarriedWoman = getMarriedWomanExpression();
+
+        System.out.println("John is male? " + isMale.interpret("John"));
+        System.out.println("Julie is a married women? " + isMarriedWoman.interpret("Married Julie"));
+    }
+
 }
