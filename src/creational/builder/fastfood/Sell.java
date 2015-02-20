f5130c81f224a6ef38d7026a6f3fbfb64bf9a7cf
Adrian Ko <adrianchko@gmail.com>
2015-02-20 07:57:11 +0000

    Produce vegmeal

--- src/creational/builder/fastfood/Sell.java
+++ src/creational/builder/fastfood/Sell.java
@@ -3,7 +3,11 @@ package creational.builder.fastfood;
 public class Sell {
     
     public static void main(String[] args) {
-        
+        Combos c = new Combos();
+        Meal vegMeal = c.prepareVegMeal();
+        System.out.println("Veg Meal");
+        vegMeal.showItems();
+        System.out.println("Total Cost: " + vegMeal.getCost());
     }
     
 }
