9b10348737fb9152ebf4084080fe3a780dcab155
Adrian Ko <adrianchko@gmail.com>
2015-02-20 07:54:45 +0000

    Create vegmeal

--- src/creational/builder/fastfood/Combos.java
+++ src/creational/builder/fastfood/Combos.java
@@ -1,4 +1,13 @@
 package creational.builder.fastfood;
 
 public class Combos {
+
+    public Meal prepareVegMeal() {
+        Meal m = new Meal();
+        m.addItem(new VegBurger());
+        m.addItem(new Coke());
+        
+        return m;
+    }
+    
 }
