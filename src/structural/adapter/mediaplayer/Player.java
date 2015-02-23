cd839af97225a3cde2f864a1e92f992bab79b70b
Adrian Ko <adrianchko@gmail.com>
2015-02-23 07:36:30 +0000

    Play mp3 file

--- src/structural/adapter/mediaplayer/Player.java
+++ src/structural/adapter/mediaplayer/Player.java
@@ -3,7 +3,9 @@ package structural.adapter.mediaplayer;
 public class Player {
     
     public static void main(String[] args) {
+        AudioPlayer ap = new AudioPlayer();
         
+        ap.play("mp3", "Secrets.mp3");
     }
     
 }
