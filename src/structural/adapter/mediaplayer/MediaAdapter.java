83127466cc0315f3bde78166a622c5c3e54ee182
Adrian Ko <adrianchko@gmail.com>
2015-02-23 07:33:58 +0000

    Create constructor for MediaAdapter

--- src/structural/adapter/mediaplayer/MediaAdapter.java
+++ src/structural/adapter/mediaplayer/MediaAdapter.java
@@ -4,12 +4,20 @@ public class MediaAdapter implements MediaPlayer {
     
     AdvancedMediaPlayer amp;
     
+    public MediaAdapter(String audioType) {
+        if (audioType.equalsIgnoreCase("vlc")) {
+            amp = new VlcPlayer();
+        } else if (audioType.equalsIgnoreCase("mp4")) {
+            amp = new Mp4Player();
+        }
+    }
+    
     @Override
     public void play(String audioType, String fileName) {
         if (audioType.equalsIgnoreCase("vlc")) {
-            
+            amp.playVlc(fileName);
         } else if (audioType.equalsIgnoreCase("mp4")) {
-            
+            amp.playMp4(fileName);
         }
     }
     
