30a757d83c91cd6532d12bbad590109b521efa22
Adrian Ko <adrianchko@gmail.com>
2015-02-23 07:27:44 +0000

    Add methods

--- src/structural/adapter/mediaplayer/AdvancedMediaPlayer.java
+++ src/structural/adapter/mediaplayer/AdvancedMediaPlayer.java
@@ -1,4 +1,8 @@
 package structural.adapter.mediaplayer;
 
 public interface AdvancedMediaPlayer {
+    
+    public void playVlc(String fileName);
+    public void playMp4(String fileName);
+    
 }
