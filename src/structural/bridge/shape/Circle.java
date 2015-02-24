80db7daeacbccbd232beecfc27747b538451406c
Adrian Ko <adrianchko@gmail.com>
2015-02-24 07:35:12 +0000

    Add members and override

---
+++ src/structural/bridge/shape/Circle.java
@@ -0,0 +1,12 @@
+package structural.bridge.shape;
+
+public class Circle extends Shape {
+    
+    private int x, y, radius;
+    
+    @Override
+    public void draw() {
+        
+    }
+    
+}
