fcb73945e078f708f9b2625b1c679afd551e8d78
Adrian Ko <adrianchko@gmail.com>
2015-02-24 07:36:12 +0000

    Add constructor

--- src/structural/bridge/shape/Circle.java
+++ src/structural/bridge/shape/Circle.java
@@ -4,9 +4,16 @@ public class Circle extends Shape {
     
     private int x, y, radius;
     
+    public Circle(int x, int y, int r, DrawAPI da) {
+        super(da);
+        this.x = x;
+        this.y = y;
+        radius = r;
+    }
+    
     @Override
     public void draw() {
-        
+        drawAPI.drawCircle(radius, x, y);
     }
     
 }
