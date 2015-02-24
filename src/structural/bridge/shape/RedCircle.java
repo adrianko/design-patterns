078adec7fd05159b409f6f1c0bb1d1c0d1f5a736
Adrian Ko <adrianchko@gmail.com>
2015-02-24 07:31:07 +0000

    Override draw method

--- src/structural/bridge/shape/RedCircle.java
+++ src/structural/bridge/shape/RedCircle.java
@@ -1,4 +1,10 @@
 package structural.bridge.shape;
 
-public class RedCircle {
+public class RedCircle implements DrawAPI {
+    
+    @Override
+    public void drawCircle(int radius, int x, int y) {
+        System.out.println("Drawing circle [ colour: red, radius: " + radius + " x: " + x + " y: " + y + "]");
+    }
+    
 }
