5575b4b1d69b3eb26a269cc19e20e659c2f8f7ae
Adrian Ko <adrianchko@gmail.com>
2015-02-24 07:39:38 +0000

    Add Bridge explanation

--- src/structural/bridge/shape/Draw.java
+++ src/structural/bridge/shape/Draw.java
@@ -1,7 +1,14 @@
 package structural.bridge.shape;
 
 public class Draw {
-    
+
+    /**
+     * Bridge - decouple an abstraction from its implementation so that two can vary independently.
+     * Involves an interface acting as a bridge which makes the functionality of concrete classes
+     * independent from interface implementer classes. Both types can be altered structurally without
+     * affecting each other.
+     * @param args
+     */
     public static void main(String[] args) {
         Shape red = new Circle(100, 100, 10, new RedCircle());
         Shape green = new Circle(100, 100, 10, new GreenCircle());
