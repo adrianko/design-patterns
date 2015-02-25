741b2b46f21cb26d96129d963c9715d10f9c527d
Adrian Ko <adrianchko@gmail.com>
2015-02-25 21:33:46 +0000

    Store and retrieve states

--- src/behavioral/memento/state/Caretaker.java
+++ src/behavioral/memento/state/Caretaker.java
@@ -7,4 +7,12 @@ public class Caretaker {
     
     private List<Memento> mementos = new ArrayList<>();
     
+    public void add(Memento s) {
+        mementos.add(s);
+    }
+    
+    public Memento get(int i) {
+        return mementos.get(i);
+    }
+    
 }
