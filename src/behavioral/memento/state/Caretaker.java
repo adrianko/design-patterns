9074ef276714d7f830a17ca84e91d40df6a9e27f
Adrian Ko <adrianchko@gmail.com>
2015-02-25 21:32:54 +0000

    Add Memento storage

--- src/behavioral/memento/state/Caretaker.java
+++ src/behavioral/memento/state/Caretaker.java
@@ -1,4 +1,10 @@
 package behavioral.memento.state;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Caretaker {
+    
+    private List<Memento> mementos = new ArrayList<>();
+    
 }
