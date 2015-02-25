a1a2e41a9441cddaf44f2ff33368fae25dd6a4e5
Adrian Ko <adrianchko@gmail.com>
2015-02-25 21:35:13 +0000

    State main

---
+++ src/behavioral/memento/state/State.java
@@ -0,0 +1,25 @@
+package behavioral.memento.state;
+
+public class State {
+    
+    public static void main(String[] args) {
+        Originator originator = new Originator();
+        Caretaker careTaker = new Caretaker();
+
+        originator.setState("State #1");
+        originator.setState("State #2");
+        careTaker.add(originator.saveState());
+
+        originator.setState("State #3");
+        careTaker.add(originator.saveState());
+
+        originator.setState("State #4");
+        System.out.println("Current State: " + originator.getState());
+
+        originator.getState(careTaker.get(0));
+        System.out.println("First saved State: " + originator.getState());
+        originator.getState(careTaker.get(1));
+        System.out.println("Second saved State: " + originator.getState());
+    }
+    
+}
