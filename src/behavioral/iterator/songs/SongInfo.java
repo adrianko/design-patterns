e03faf174a3cbd945dcecd60acb439bd3a8d4ebe
Adrian Ko <adrianchko@gmail.com>
2015-02-27 07:28:16 +0000

    Add constructor

--- src/behavioral/iterator/songs/SongInfo.java
+++ src/behavioral/iterator/songs/SongInfo.java
@@ -5,5 +5,11 @@ public class SongInfo {
     String songName;
     String bandName;
     int yearReleased;
+
+    public SongInfo(String newSongName, String newBandName, int newYearReleased) {
+        songName = newSongName;
+        bandName = newBandName;
+        yearReleased = newYearReleased;
+    }
     
 }
