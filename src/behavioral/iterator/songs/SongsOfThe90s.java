e8085dc00a2b218d38f41dbddcdde05ec539fba9
Adrian Ko <adrianchko@gmail.com>
2015-02-27 07:40:03 +0000

    Add constructor

--- src/behavioral/iterator/songs/SongsOfThe90s.java
+++ src/behavioral/iterator/songs/SongsOfThe90s.java
@@ -8,6 +8,12 @@ public class SongsOfThe90s implements SongIterator {
     Hashtable<Integer, SongInfo> bestSongs = new Hashtable<>();
     int hashKey = 0;
 
+    public SongsOfThe90s() {
+        addSong("Losing My Religion", "REM", 1991);
+        addSong("Creep", "Radiohead", 1993);
+        addSong("Walk on the Ocean", "Toad The Wet Sprocket", 1991);
+    }
+
     public void addSong(String songName, String bandName, int yearReleased){
         SongInfo songInfo = new SongInfo(songName, bandName, yearReleased);
         bestSongs.put(hashKey, songInfo);
