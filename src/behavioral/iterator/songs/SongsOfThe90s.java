1789423565796e8979635b75ab6084e0c251852b
Adrian Ko <adrianchko@gmail.com>
2015-02-27 07:39:47 +0000

    Add getters and setters

--- src/behavioral/iterator/songs/SongsOfThe90s.java
+++ src/behavioral/iterator/songs/SongsOfThe90s.java
@@ -5,7 +5,18 @@ import java.util.Iterator;
 
 public class SongsOfThe90s implements SongIterator {
 
-    Hashtable<Integer, SongInfo> bestSongs = new Hashtable<Integer, SongInfo>();
+    Hashtable<Integer, SongInfo> bestSongs = new Hashtable<>();
+    int hashKey = 0;
+
+    public void addSong(String songName, String bandName, int yearReleased){
+        SongInfo songInfo = new SongInfo(songName, bandName, yearReleased);
+        bestSongs.put(hashKey, songInfo);
+        hashKey++;
+    }
+    
+    public Hashtable<Integer, SongInfo> getBestSongs(){
+        return bestSongs;
+    }
     
     @Override
     public Iterator createIterator() {
