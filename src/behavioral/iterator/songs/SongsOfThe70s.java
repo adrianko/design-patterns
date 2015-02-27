081fa543493ea446c236f2e4ff19faf500315646
Adrian Ko <adrianchko@gmail.com>
2015-02-27 07:33:13 +0000

    Add getter and setter

--- src/behavioral/iterator/songs/SongsOfThe70s.java
+++ src/behavioral/iterator/songs/SongsOfThe70s.java
@@ -1,4 +1,12 @@
     ArrayList<SongInfo> bestSongs;
+    public void addSong(String songName, String bandName, int yearReleased){
+        SongInfo songInfo = new SongInfo(songName, bandName, yearReleased);
+        bestSongs.add(songInfo);
+    }
+    
+    public ArrayList<SongInfo> getBestSongs(){
+        return bestSongs;
+    }
     @Override
     public Iterator createIterator() {
         return bestSongs.iterator();
