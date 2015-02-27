b75720a0c1504b8bb110e222337b04f5329fc97b
Adrian Ko <adrianchko@gmail.com>
2015-02-27 07:36:24 +0000

    Add member and override

--- src/behavioral/iterator/songs/SongsOfThe80s.java
+++ src/behavioral/iterator/songs/SongsOfThe80s.java
@@ -1,4 +1,15 @@
 package behavioral.iterator.songs;
 
-public class SongsOfThe80s {
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class SongsOfThe80s implements SongIterator {
+    
+    SongInfo[] bestSongs;
+    
+    @Override
+    public Iterator createIterator() {
+        return Arrays.asList(bestSongs).iterator();
+    }
+    
 }
