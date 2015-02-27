94da5b24da842d434225f71671be973288de2fa2
Adrian Ko <adrianchko@gmail.com>
2015-02-27 07:41:19 +0000

    Add constructor

--- src/behavioral/iterator/songs/DJ.java
+++ src/behavioral/iterator/songs/DJ.java
@@ -5,5 +5,11 @@ public class DJ {
     SongIterator iter70sSongs;
     SongIterator iter80sSongs;
     SongIterator iter90sSongs;
-    
+
+    public DJ(SongIterator newSongs70s, SongIterator newSongs80s, SongIterator newSongs90s) {
+        iter70sSongs = newSongs70s;
+        iter80sSongs = newSongs80s;
+        iter90sSongs = newSongs90s;
+    }
+
 }
