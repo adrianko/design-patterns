22a7e275d2c6319554a63c36c924df584d051fc1
Adrian Ko <adrianchko@gmail.com>
2015-02-27 07:45:13 +0000

    Add main

--- src/behavioral/iterator/songs/Radio.java
+++ src/behavioral/iterator/songs/Radio.java
@@ -1,4 +1,10 @@
 package behavioral.iterator.songs;
 
 public class Radio {
+    
+    public static void main(String[] args) {
+        DJ ak = new DJ(new SongsOfThe70s(), new SongsOfThe80s(), new SongsOfThe90s());
+        ak.showTheSongs();
+    }
+    
 }
