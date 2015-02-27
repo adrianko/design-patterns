fc905b7e0332bbd96786856a440848525c88de79
Adrian Ko <adrianchko@gmail.com>
2015-02-27 07:38:57 +0000

    Add member and iterator

--- src/behavioral/iterator/songs/SongsOfThe90s.java
+++ src/behavioral/iterator/songs/SongsOfThe90s.java
@@ -1,4 +1,15 @@
 package behavioral.iterator.songs;
 
-public class SongsOfThe90s {
+import java.util.Hashtable;
+import java.util.Iterator;
+
+public class SongsOfThe90s implements SongIterator {
+
+    Hashtable<Integer, SongInfo> bestSongs = new Hashtable<Integer, SongInfo>();
+    
+    @Override
+    public Iterator createIterator() {
+        return bestSongs.values().iterator();
+    }
+    
 }
