e277c56cd04801e55a8ae126d5da7f6481db175d
Adrian Ko <adrianchko@gmail.com>
2015-02-27 07:33:21 +0000

    Add constructor

--- src/behavioral/iterator/songs/SongsOfThe70s.java
+++ src/behavioral/iterator/songs/SongsOfThe70s.java
@@ -1,4 +1,20 @@
+package behavioral.iterator.songs;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class SongsOfThe70s implements SongIterator {
+    
     ArrayList<SongInfo> bestSongs;
+
+    public SongsOfThe70s() {
+        bestSongs = new ArrayList<>();
+        addSong("Imagine", "John Lennon", 1971);
+        addSong("American Pie", "Don McLean", 1971);
+        addSong("I Will Survive", "Gloria Gaynor", 1979);
+
+    }
+
     public void addSong(String songName, String bandName, int yearReleased){
         SongInfo songInfo = new SongInfo(songName, bandName, yearReleased);
         bestSongs.add(songInfo);
@@ -7,7 +23,10 @@
     public ArrayList<SongInfo> getBestSongs(){
         return bestSongs;
     }
+    
     @Override
     public Iterator createIterator() {
         return bestSongs.iterator();
     }
+    
+}
