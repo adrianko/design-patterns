16f48ab5b0c8d4bf9c6755b3720cf26df938d071
Adrian Ko <adrianchko@gmail.com>
2015-02-27 07:46:34 +0000

    Add explanation of iterator

--- src/behavioral/iterator/songs/Radio.java
+++ src/behavioral/iterator/songs/Radio.java
@@ -1,7 +1,12 @@
 package behavioral.iterator.songs;
 
 public class Radio {
-    
+
+    /**
+     * Iterator - allows access to objects that are stored in different types of collections. Done
+     * through the use of a common interface which provides an iterator.
+     * @param args
+     */
     public static void main(String[] args) {
         DJ ak = new DJ(new SongsOfThe70s(), new SongsOfThe80s(), new SongsOfThe90s());
         ak.showTheSongs();
