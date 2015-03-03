3d719d3c3735f14300d454f9b90009b3e663bd3c
Adrian Ko <adrianchko@gmail.com>
2015-03-03 09:24:02 +0000

    Add main content

--- src/behavioral/state/atm/Customer.java
+++ src/behavioral/state/atm/Customer.java
@@ -3,7 +3,14 @@ package behavioral.state.atm;
 public class Customer {
     
     public static void main(String[] args) {
-        
+        ATMMachine atm = new ATMMachine();
+        atm.insertCard();
+        atm.ejectCard();
+        atm.insertCard();
+        atm.insertPin(1234);
+        atm.requestCash(2000);
+        atm.insertCard();
+        atm.insertPin(1234);
     }
     
 }
