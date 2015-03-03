2c5559169da35a6dd8db7ca2b3d03e8714ed8c8c
Adrian Ko <adrianchko@gmail.com>
2015-03-03 09:25:59 +0000

    Add explanation of state pattern

--- src/behavioral/state/atm/Customer.java
+++ src/behavioral/state/atm/Customer.java
@@ -1,7 +1,13 @@
 package behavioral.state.atm;
 
 public class Customer {
-    
+
+    /**
+     * State - used to encapsulate varying behaviour for the same routine based on an object's state object.
+     * Is a cleaner way for an object to change its behaviour at runtime without resorting to large monolithic
+     * conditional statements.
+     * @param args
+     */
     public static void main(String[] args) {
         ATMMachine atm = new ATMMachine();
         atm.insertCard();
