0af4eef57f52fb84a045b5c886aee0926999d7b3
Adrian Ko <adrianchko@gmail.com>
2015-03-03 07:55:22 +0000

    Add method overrides

--- src/behavioral/state/atm/HasCard.java
+++ src/behavioral/state/atm/HasCard.java
@@ -6,22 +6,32 @@ public class HasCard implements ATMState {
     
     @Override
     public void insertCard() {
-        
+        System.out.println("You can only insert one card at a time");
     }
 
     @Override
     public void ejectCard() {
-
-    }
-
-    @Override
-    public void insertPin(int pinEntered) {
-
+        System.out.println("Your card is ejected");
+        atmMachine.setATMState(atmMachine.getNoCardState());
     }
 
     @Override
     public void requestCash(int cashToWithdraw) {
-
+        System.out.println("You have not entered your PIN");
     }
-    
+
+    @Override
+    public void insertPin(int pinEntered) {
+        if (pinEntered == 1234) {
+            System.out.println("You entered the correct PIN");
+            atmMachine.correctPinEntered = true;
+            atmMachine.setATMState(atmMachine.getHasPin());
+        } else {
+            System.out.println("You entered the wrong PIN");
+            atmMachine.correctPinEntered = false;
+            System.out.println("Your card is ejected");
+            atmMachine.setATMState(atmMachine.getNoCardState());
+        }
+    }
+
 }
