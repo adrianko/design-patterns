7b2a1f31df0d413818edca24c4ca4d63666fc6d9
Adrian Ko <adrianchko@gmail.com>
2015-03-03 07:51:18 +0000

    Add members

--- src/behavioral/state/atm/ATMMachine.java
+++ src/behavioral/state/atm/ATMMachine.java
@@ -1,4 +1,15 @@
 package behavioral.state.atm;
 
 public class ATMMachine {
+    
+    ATMState hasCard;
+    ATMState noCard;
+    ATMState hasCorrectPin;
+    ATMState atmOutOfMoney;
+
+    ATMState atmState;
+
+    int cashInMachine = 2000;
+    boolean correctPinEntered = false;
+
 }
