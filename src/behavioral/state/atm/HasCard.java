cff7c4077d43d7c3fa140450333a1f3f3d52e509
Adrian Ko <adrianchko@gmail.com>
2015-03-03 07:53:52 +0000

    HasCard ATMState with overridden methods

---
+++ src/behavioral/state/atm/HasCard.java
@@ -0,0 +1,25 @@
+package behavioral.state.atm;
+
+public class HasCard implements ATMState {
+    
+    @Override
+    public void insertCard() {
+        
+    }
+
+    @Override
+    public void ejectCard() {
+
+    }
+
+    @Override
+    public void insertPin(int pinEntered) {
+
+    }
+
+    @Override
+    public void requestCash(int cashToWithdraw) {
+
+    }
+    
+}
