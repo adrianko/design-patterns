ef7759e40d28c37391700764d9d12ce1f28ebc7f
Adrian Ko <adrianchko@gmail.com>
2015-03-03 09:22:07 +0000

    Add member and constructor

--- src/behavioral/state/atm/NoCash.java
+++ src/behavioral/state/atm/NoCash.java
@@ -1,4 +1,10 @@
 package behavioral.state.atm;
 
 public class NoCash implements ATMState {
+
+    ATMMachine atmMachine;
+
+    public NoCash(ATMMachine newATMMachine){
+        atmMachine = newATMMachine;
+    }
 }
