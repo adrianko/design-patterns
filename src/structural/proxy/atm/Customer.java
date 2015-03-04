1cd45e953a4240d7d356b40bbb468f3a59da236e
Adrian Ko <adrianchko@gmail.com>
2015-03-04 07:41:34 +0000

    Add explanation of proxy

--- src/structural/proxy/atm/Customer.java
+++ src/structural/proxy/atm/Customer.java
@@ -2,6 +2,10 @@ package structural.proxy.atm;
 
 public class Customer {
 
+    /**
+     * Proxy - limits access to just the methods made accessible in another class.
+     * @param args
+     */
     public static void main(String[] args) {
         GetATMData realATMMachine = new ATMMachine();
         GetATMData atmProxy = new ATMProxy();
