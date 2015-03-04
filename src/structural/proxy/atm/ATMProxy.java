f862c45245fdd4c8775f93356143423aa8df53b3
Adrian Ko <adrianchko@gmail.com>
2015-03-04 07:37:13 +0000

    Create ATMProxy class

---
+++ src/structural/proxy/atm/ATMProxy.java
@@ -0,0 +1,15 @@
+package structural.proxy.atm;
+
+public class ATMProxy implements GetATMData {
+    
+    @Override
+    public ATMState getATMState() {
+        return null;
+    }
+
+    @Override
+    public int getCashInMachine() {
+        return 0;
+    }
+    
+}
