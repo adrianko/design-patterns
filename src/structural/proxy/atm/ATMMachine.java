27d0b8980e9d79c006dd40a5f36c6dd3525a87fe
Adrian Ko <adrianchko@gmail.com>
2015-03-04 07:34:07 +0000

    Add members to ATMMachine

--- src/structural/proxy/atm/ATMMachine.java
+++ src/structural/proxy/atm/ATMMachine.java
@@ -1,4 +1,20 @@
 package structural.proxy.atm;
 
-public class ATMMachine {
+public class ATMMachine implements GetATMData {
+
+    ATMState hasCard;
+    ATMState noCard;
+    ATMState hasCorrectPin;
+    ATMState atmOutOfMoney;
+    
+    @Override
+    public ATMState getATMState() {
+        return null;
+    }
+
+    @Override
+    public int getCashInMachine() {
+        return 0;
+    }
+    
 }
