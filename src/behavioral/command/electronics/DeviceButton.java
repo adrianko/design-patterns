340a0c247dc1c45d7a8019f15d86bf45a13fc12a
Adrian Ko <adrianchko@gmail.com>
2015-03-05 07:54:20 +0000

    Add Device Button implementation

---
+++ src/behavioral/command/electronics/DeviceButton.java
@@ -0,0 +1,19 @@
+package behavioral.command.electronics;
+
+public class DeviceButton {
+
+    Command theCommand;
+
+    public DeviceButton(Command newCommand) {
+        theCommand = newCommand;
+    }
+
+    public void press() {
+        theCommand.execute();
+    }
+    
+    public void pressUndo() {
+        theCommand.undo();
+    }
+
+}
