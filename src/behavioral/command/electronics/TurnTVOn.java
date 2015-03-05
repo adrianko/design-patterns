7967229b6c78e945b06cc3dce0e3ae71e7f8ac9d
Adrian Ko <adrianchko@gmail.com>
2015-03-05 07:51:35 +0000

    Add TV On command

---
+++ src/behavioral/command/electronics/TurnTVOn.java
@@ -0,0 +1,19 @@
+package behavioral.command.electronics;
+
+public class TurnTVOn implements Command {
+
+    ElectronicDevice theDevice;
+
+    public TurnTVOn(ElectronicDevice newDevice){
+        theDevice = newDevice;
+    }
+
+    public void execute() {
+        theDevice.on();
+    }
+
+    public void undo() {
+        theDevice.off();
+    }
+
+}
