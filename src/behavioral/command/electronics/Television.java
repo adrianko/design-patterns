4b687599f4062227b7a381f0a66bd3cdc63c43a5
Adrian Ko <adrianchko@gmail.com>
2015-03-05 07:48:53 +0000

    Implement interface methods

--- src/behavioral/command/electronics/Television.java
+++ src/behavioral/command/electronics/Television.java
@@ -3,5 +3,23 @@ package behavioral.command.electronics;
 public class Television {
 
     private int volume = 0;
+    
+    public void on() {
+        System.out.println("TV is on");
+    }
+
+    public void off() {
+        System.out.println("TV is off");
+    }
+
+    public void volumeUp() {
+        volume++;
+        System.out.println("TV Volume is at: " + volume);
+    }
+
+    public void volumeDown() {
+        volume--;
+        System.out.println("TV Volume is at: " + volume);
+    }
 
 }
