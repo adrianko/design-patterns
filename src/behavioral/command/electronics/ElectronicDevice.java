9d21ff6e2aee156ebe32137b7d67c5db2e0d88e7
Adrian Ko <adrianchko@gmail.com>
2015-03-05 07:47:31 +0000

    Add methods

--- src/behavioral/command/electronics/ElectronicDevice.java
+++ src/behavioral/command/electronics/ElectronicDevice.java
@@ -1,4 +1,10 @@
 package behavioral.command.electronics;
 
 public interface ElectronicDevice {
+
+    public void on();
+    public void off();
+    public void volumeUp();
+    public void volumeDown();
+    
 }
