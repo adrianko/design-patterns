7897135ccdb0487f40a89946793cf8687fa42334
Adrian Ko <adrianchko@gmail.com>
2015-03-05 07:53:19 +0000

    Implement command interface in tv up

--- src/behavioral/command/electronics/TurnTVUp.java
+++ src/behavioral/command/electronics/TurnTVUp.java
@@ -1,4 +1,19 @@
 package behavioral.command.electronics;
 
-public class TurnTVUp {
+public class TurnTVUp implements Command {
+
+    ElectronicDevice theDevice;
+
+    public TurnTVUp(ElectronicDevice newDevice){
+        theDevice = newDevice;
+    }
+
+    public void execute() {
+        theDevice.volumeUp();
+    }
+
+    public void undo() {
+        theDevice.volumeDown();
+    }
+
 }
